514335abb49f4ebf129c531cb16911681934a438
Михайличенко Александр <meadow174@gmail.com>
2024-12-02 17:45:06 +0500

    Done task6

--- src/shared/modules/offer/offer.container.ts
+++ src/shared/modules/offer/offer.container.ts
@@ -4,10 +4,13 @@ import { Component } from '../../types/index.js';
 import { DefaultOfferService } from './default-offer.service.js';
 import { OfferEntity, OfferModel } from './offer.entity.js';
 import { types } from '@typegoose/typegoose';
+import { BaseController } from '../../libs/rest/index.js';
+import OfferController from './offer.controller.js';
 
 export function createOfferContainer() {
   const offerContainer = new Container();
   offerContainer.bind<OfferService>(Component.OfferService).to(DefaultOfferService);
   offerContainer.bind<types.ModelType<OfferEntity>>(Component.OfferModel).toConstantValue(OfferModel);
+  offerContainer.bind<BaseController>(Component.OfferController).to(OfferController).inSingletonScope();
   return offerContainer;
 }
---
+++ src/shared/modules/offer/rdo/offer.rdo.ts
@@ -0,0 +1,55 @@
+import { Expose } from 'class-transformer';
+import { City } from '../../../types/city.enum.js';
+import { User } from '../../../types/user.type.js';
+import { AccommodationType } from '../../../types/accommodation-type.enum.js';
+import { Convenience } from '../../../types/convenience.enum.js';
+
+export class OfferRdo {
+  @Expose()
+  public title!: string;
+
+  @Expose()
+  public description!: string;
+
+  @Expose()
+  public createdDate!: Date;
+
+  @Expose()
+  public city!: City;
+
+  @Expose()
+  public previewImage!: string;
+
+  @Expose()
+  public offerImages!: string[];
+
+  @Expose()
+  public isPremium!: boolean;
+
+  @Expose()
+  public isFavorite!: boolean;
+
+  @Expose()
+  public rating!: number;
+
+  @Expose()
+  public type!: AccommodationType;
+
+  @Expose()
+  public roomCount!: number;
+
+  @Expose()
+  public guestCount!: number;
+
+  @Expose()
+  public price!: number;
+
+  @Expose()
+  public conveniences!: Convenience[];
+
+  @Expose()
+  public user!: User;
+
+  @Expose()
+  public coordinates!: string;
+}
